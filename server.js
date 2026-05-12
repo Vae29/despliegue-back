2cae041f9acd6c7ccb702915a56fd97073d07aa1
Vae29 <valesalvalen@gmail.com>
2026-05-12 12:00:43 -0500

    cambios link netlify

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://silly-arithmetic-8ccdb6.netlify.app/']
+  origin: ['http://localhost:5173', 'https://silly-arithmetic-8ccdb6.netlify.app']
 }));
 
 app.use(express.json());
